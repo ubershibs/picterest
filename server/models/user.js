e09310154168cba74cea4728fdab7222f80bcfcd
ubershibs <luke.walker@gmail.com>
2016-08-10 21:17:56 -0400

    Removed Login button in favour of strategy-specific buttons

--- server/models/user.js
+++ server/models/user.js
@@ -7,24 +7,12 @@ var moment = require('moment');
 //user Schema
 var userSchema = new Schema({
   username: String,
-  provider: String,
-  authID: {type: String, index: true },
+  githubId: {type: String, index: true },
   email: { type: String, index: true },
   password: { type: String, select: false },
   accessToken: String
 });
 
 userSchema.plugin(findOrCreate);
-
-userSchema.methods.createToken = function() {
-  // set expiration to 60 days
-  var payload = {
-      exp: moment().add(14, 'days').unix(),
-      iat: moment().unix(),
-      sub: user._id
-    };
-
-  return jwt.encode(payload, config.tokenSecret);
-};
 
 mongoose.model('User', userSchema);
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,16 +5,15 @@ var moment = require('moment');
 var jwt = require('jwt-simple');
 
 var config = require('./config');
-var User = mongoose.model('User')
 var BoardHandler = require('../handlers/BoardHandler');
 var UserHandler = require('../handlers/UserHandler');
 
-
+// Auth routes
 router.post('/auth/login', UserHandler.localSignin);
 router.post('/auth/signup', UserHandler.localSignup);
 router.post('/auth/github', UserHandler.githubSignin)
 
-
+// Board routes
 router.get('/api/pics', BoardHandler.getAllThePics);
 router.post('/api/pics', isAuthenticated, BoardHandler.postPic);
 router.post('/api/pic/:id/like', isAuthenticated, BoardHandler.likePic)
